dfa8e6afea14fb3fcd932b0429d852fcda0798fa
caroline09ops <caroline09.cardenas@gmail.com>
2026-03-27 16:26:45 -0500

    fix rest framework render

--- drfsimplecrud/settings.py
+++ drfsimplecrud/settings.py
@@ -106,3 +106,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # 🔢 DEFAULT PRIMARY KEY
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework.renderers.JSONRenderer',
+    ]
+}
