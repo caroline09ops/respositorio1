a2bc79a33974ce34b4d71a12926b4c6b8763416e
caroline09ops <caroline09.cardenas@gmail.com>
2026-03-27 16:43:08 -0500

    redirigir a api

--- drfsimplecrud/urls.py
+++ drfsimplecrud/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse  
-
+from django.shortcuts import redirect  # 👈 IMPORTANTE
 
 urlpatterns = [
+    path('', lambda request: redirect('/api/projects/')),  # 👈 REDIRECCIÓN
     path('admin/', admin.site.urls),
     path('api/', include('projects.urls')),
 ]
